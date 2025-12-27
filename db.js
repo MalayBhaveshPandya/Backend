19495b1a20fd442c47dacf1bf07f68b971e0ce1a
MalayBhaveshPandya <c.35.malay.pandya@gmail.com>
2025-12-27 23:38:02 +0530

    Fixing3

--- db.js
+++ db.js
@@ -12,8 +12,9 @@ async function connectToMongo() {
   if (!cached.promise) {
     const start = Date.now();
     cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      family: 4 // Use IPv4, skip trying IPv6
     }).then((m) => {
       const ms = Date.now() - start;
       console.log(`✅ MongoDB connected in ${ms}ms`);
